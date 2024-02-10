6edd25eb60e0b4750bbf9ff4d488ee4d895df64e
Anton Khrapov <froe054@mail.ru>
2024-02-10 15:22:21 +0700

    Initial Commit

---
+++ Task4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task4
+//
+//  Created by Anton on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
